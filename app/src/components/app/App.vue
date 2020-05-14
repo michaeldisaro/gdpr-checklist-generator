8e2e02e815f87eeb991c99994f62b5d051491ec4
michaeldisaro <djmds@email.it>
2020-05-14 18:32:13 +0200

    fixed logo

--- app/src/components/app/App.vue
+++ app/src/components/app/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app id="app" style="background-color:#003399">
     <v-app-bar class="pa-1" style="background-color:#003399" height="72px" fixed>
-      <v-img src="/logo.png" class="mr-5" max-width="64px" max-height="64px" contain/>
+      <v-img src="logo.png" class="mr-5" max-width="64px" max-height="64px" contain/>
       <h1>Easy GDPR Checklist</h1>
     </v-app-bar>
     <v-content class="mt-12 pa-10">
